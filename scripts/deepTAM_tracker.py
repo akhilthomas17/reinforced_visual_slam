6741b34c8b696a32d36d648e86199d6e85f8271c
Akhil Thomas <thomas.akhil17@gmail.com>
2018-01-16 17:38:42 +0100

    Modified the request messages to the format required by deepTAM placeholder

--- scripts/deepTAM_tracker.py
+++ scripts/deepTAM_tracker.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from dummy_tf_node.srv import *
+from reinforced_visual_slam.srv import *
 import rospy
 import tensorflow as tf
+
 from cv_bridge import CvBridge
 import cv2
+from PIL import Image
+
+from minieigen import Quaternion
+
 from depthmotionnet.vis import angleaxis_to_rotation_matrix
 from deepTAM.evaluation.helpers import *
 from tfutils.helpers import optimistic_restore
@@ -18,7 +23,7 @@ class DeepTAMTracker(object):
         gpu_options.per_process_gpu_memory_fraction=0.8
         self.session = tf.InteractiveSession(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options))
         self.session.run(tf.global_variables_initializer())
-	self._cv_bridge = CvBridge()
+        self._cv_bridge = CvBridge()
 
     def configure(self, network_script_path, network_session_path):
         print("Configuring the deepTAM network")
@@ -28,20 +33,71 @@ class DeepTAMTracker(object):
         self.output = self.tracking_net.build_net(**self.tracking_net.placeholders)
         optimistic_restore(self.session, network_session_path, verbose=True)
 
-    def track_image_cb(self, req):
-        print("Returning response")
-	keyframe_depth = self._cv_bridge.imgmsg_to_cv2(req.keyframe_depth, "bgr8")
+    def track_image_cb(self, req, test=False):
+        print("Recieved image to track")
+
+        # Requirement: Depth has to np array float32, in meters, with shape (1,1,240,320)
+        # Get the Keyframe depth image and convert it to openCV Image format (Assuming Depth in Meters)
+        try:
+            keyframe_depth = self._cv_bridge.imgmsg_to_cv2(req.keyframe_depth, "passthrough")
+        except CvBridgeError as e:
+            print(e)
+            return TrackImageResponse(0)
+        # Resize Keyframe depth image to (320W,240H)
+        keyframe_depth = cv2.resize(keyframe_depth, (320, 240))
+        if test:
+            cv2.imshow('CV Keyframe Depth',keyframe_depth)
+            cv2.waitKey(3)
+        # Convert Keyframe depth image to Float32 
+        keyframe_depth = keyframe_depth.astype('float32')
+        # Find inverse depth
         key_inv_depth = 1/keyframe_depth
+
+        # Requirement: RGB has to np array float32, normalized in range (-0.5,0.5), with shape (1,3,240,320)
+        # Get the Keyframe RGB image from ROS message and convert it to openCV Image format
+        try:
+            keyframe_image = self._cv_bridge.imgmsg_to_cv2(req.keyframe_image, "passthrough")
+        except CvBridgeError as e:
+            print(e)
+            return TrackImageResponse(0)
+        if test:
+            cv2.imshow('CV Keyframe RGB', keyframe_image)
+            cv2.waitKey(3)
+        # Normalize Keyframe RGB image to range (-0.5, 0.5) and save as Float32
+        keyframe_image = cv2.normalize(keyframe_image, None, alpha=-0.5, beta=0.5, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        # Resize Keyframe RGB image to (320W,240H) and reshape it to (3, 240, 320)
+        keyframe_image = cv2.resize(keyframe_image, (320, 240))
+        keyframe_image = np.transpose(keyframe_image, (2,0,1))
+
+        # Requirement: RGB has to np array float32, normalized in range (-0.5,0.5), with shape (1,3,240,320)
+        # Get the Current RGB image from ROS message and convert it to openCV Image format
+        try:
+            current_image = self._cv_bridge.imgmsg_to_cv2(req.current_image, "passthrough")
+        except CvBridgeError as e:
+            print(e)
+            return TrackImageResponse(0)
+        if test:
+            cv2.imshow('CV Current RGB', current_image)
+            cv2.waitKey(3)
+        # Normalize Current RGB image to range (-0.5, 0.5) and save as Float32
+        current_image = cv2.normalize(current_image, None, alpha=-0.5, beta=0.5, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        # Resize Current RGB image to (320W,240H) and reshape it to (3, 240, 320)
+        current_image = cv2.resize(current_image, (320, 240))
+        current_image = np.transpose(current_image, (2,0,1))
+
+        # Loading prior rotation and translation
+        prev_rotation = Quaternion(req.rotation_prior[3], req.rotation_prior[0], req.rotation_prior[1], req.rotation_prior[2])
+        prev_rotation = prev_rotation.toAxisAngle()
+
+        # Loading the feed dict with converted values:
         feed_dict = {
             self.tracking_net.placeholders['depth_key']: key_inv_depth[np.newaxis,np.newaxis,:,:],
-            self.tracking_net.placeholders['image_key']: req.keyframe_image[np.newaxis,:,:,:],
-            self.tracking_net.placeholders['image_current']: req.current_image[np.newaxis,:,:,:],
+            self.tracking_net.placeholders['image_key']: keyframe_image[np.newaxis,:,:,:],
+            self.tracking_net.placeholders['image_current']: current_image[np.newaxis,:,:,:],
             self.tracking_net.placeholders['intrinsics']: req.intrinsics,
-            self.tracking_net.placeholders['prev_rotation']: req.rotation_prior,
+            self.tracking_net.placeholders['prev_rotation']: prev_rotation,
             self.tracking_net.placeholders['prev_translation']: req.translation_prior,
         }
-#        if 'depth_current' in self.tracking_net.placeholders:
-#             feed_dict[tracking_net.placeholders['depth_current']] = 1/req.keyframe_depth[np.newaxis,np.newaxis,:,:]
 
         output_arrs = self.session.run(output, feed_dict=feed_dict)
         t = output_arrs['predict_translation'][0]
@@ -49,6 +105,7 @@ class DeepTAMTracker(object):
         R_w = R.dot(req.rotation_prior)
         t_w = R.dot(req.translation_prior) + t
         
+        print("Returning tracked response")
         return TrackImageResponse(1)
 
     def run(self):
