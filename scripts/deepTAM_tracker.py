daae17d7be11cfc63e42cdcf78529230ddfc46cb
Akhil Thomas <thomas.akhil17@gmail.com>
2018-03-20 17:24:09 +0100

    Found and fixed bug with sun3d intrinsics passed to adjust_intrinsics()

--- scripts/deepTAM_tracker.py
+++ scripts/deepTAM_tracker.py
@@ -26,13 +26,14 @@ class DeepTAMTracker(object):
 
     def __init__(self, input_img_width=640, input_img_height=480):
         rospy.init_node('deepTAM_tracker')
+        self.debug = False
         
         state = rospy.Service('tracker_status', TrackerStatus, self.tracker_status_cb)
         self.status = False
-        #self.status_pub = rospy.Publisher('tracker_status', Bool, queue_size=1)
-        #self.status_pub.publish(Bool(False))
+        self.status_pub = rospy.Publisher('tracker_status', Bool, queue_size=1)
+        self.status_pub.publish(Bool(False))
 
-        print("Configuring tensorflow session")
+        rospy.loginfo("Configuring tensorflow session")
         gpu_options = tf.GPUOptions()
         gpu_options.per_process_gpu_memory_fraction=0.8
         self.session = tf.InteractiveSession(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options))
@@ -42,22 +43,22 @@ class DeepTAMTracker(object):
         # Sun 3d normalized intrinsics
         self.sun3d_intrinsics = np.array([[0.89115971, 1.18821299, 0.5, 0.5]],dtype=np.float32)
         self.sun3d_K = np.eye(3)
-        self.sun3d_K[0,0] = self.sun3d_intrinsics[0][0]*input_img_width
-        self.sun3d_K[1,1] = self.sun3d_intrinsics[0][1]*input_img_height
-        self.sun3d_K[0,2] = self.sun3d_intrinsics[0][2]*input_img_width
-        self.sun3d_K[1,2] = self.sun3d_intrinsics[0][3]*input_img_height
+        self.sun3d_K[0,0] = self.sun3d_intrinsics[0][0]*320
+        self.sun3d_K[1,1] = self.sun3d_intrinsics[0][1]*240
+        self.sun3d_K[0,2] = self.sun3d_intrinsics[0][2]*320
+        self.sun3d_K[1,2] = self.sun3d_intrinsics[0][3]*240
         self.input_img_height = input_img_height
         self.input_img_width = input_img_width
 
     def configure(self, network_script_path, network_session_path):
-        print("Configuring the deepTAM network")
+        rospy.loginfo("Loading the deepTAM network")
         tracking_mod = load_myNetworks_module_noname(network_script_path)
         self.tracking_net = tracking_mod.TrackingNetwork()
         [self.image_height, self.image_width] = self.tracking_net.placeholders['image_key'].get_shape().as_list()[-2:]
         self.output = self.tracking_net.build_net(**self.tracking_net.placeholders)
         optimistic_restore(self.session, network_session_path, verbose=True)
         self.status = True
-        #self.status_pub.publish(Bool(True))
+        self.status_pub.publish(Bool(True))
 
     def rotation_matrix_to_quaternion(self, R):
         q = Quaternion()
@@ -66,9 +67,8 @@ class DeepTAMTracker(object):
         q.y = q_mini[1]
         q.z = q_mini[2]
         q.w = q_mini[3]
-        print("Quaternion mini:", q.x, q.y, q.z, q.w)
-        # print("Quaternion py:", Quat(*(quat(matrix=R).elements)))
-        # return Quat(*(quat(matrix=R).elements))
+        if self.debug:
+            print("Quaternion mini:", q.x, q.y, q.z, q.w)
         return q
 
     def angle_axis_to_quaternion(self, axis_a):
@@ -101,14 +101,26 @@ class DeepTAMTracker(object):
 
         if not depth is None:
             depth_metric = 'camera_z'
+            if self.debug:
+                print("Size Depth before:", depth.size)
         else:
             depth_metric = None
+
+
+        if self.debug:
+            print("Size RGB before:", rgb.size)
+            print("K", K)
+            print("width", width)
+            print("height", height)
         
         rgb_view = View(R=R,t=t,K=K,image=rgb, depth=depth, depth_metric=depth_metric)
         new_rgb_view = adjust_intrinsics(rgb_view, self.sun3d_K, width, height)
         new_rgb = new_rgb_view.image
 
-        if False:
+        if self.debug:
+            print("Size RGB after:", new_rgb.size)
+            if not depth is None:
+                print("Size Depth after:", new_rgb_view.depth.size)
             new_rgb.show()
 
         # Normalize Keyframe RGB image to range (-0.5, 0.5) and save as Float32
@@ -128,12 +140,13 @@ class DeepTAMTracker(object):
     def tracker_status_cb(self, req):
         return TrackerStatusResponse(Bool(self.status))
 
-    def track_image_cb(self, req, test=False):
+    def track_image_cb(self, req, plot_debug=False):
         print("Recieved image to track")
 
         # Load the intrinsics from the request
         intrinsics = np.array(req.intrinsics)
-        #print("intrinsics", intrinsics)
+        if self.debug:
+            print("intrinsics", intrinsics)
 
         # Requirement: Depth has to np array float32, in meters, with shape (1,1,240,320)
         # Get the Keyframe depth image and convert it to openCV Image format (Assuming Depth in Meters)
@@ -144,7 +157,8 @@ class DeepTAMTracker(object):
             return TrackImageResponse(0)
 
         # Resize Keyframe depth image to (320W,240H)
-        keyframe_depth = cv2.resize(keyframe_depth, (320, 240))
+        #keyframe_depth = cv2.resize(keyframe_depth, (320, 240))
+
         # Convert Keyframe depth image to Float32 
         #keyframe_depth = keyframe_depth.astype('float32')
 
@@ -156,25 +170,26 @@ class DeepTAMTracker(object):
             print(e)
             return TrackImageResponse(0)
 
-        if False:
+        if plot_debug:
             cv2.imshow('CV Keyframe RGB', keyframe_image)
             #keyframe_image.show()
 
         # Convert CV image to PIL image and resize
         keyframe_image = cv2.cvtColor(keyframe_image, cv2.COLOR_BGR2RGB)
-        keyframe_image = Image.fromarray(keyframe_image).resize((320, 240))
+        keyframe_image = Image.fromarray(keyframe_image)
 
         keyframe_image_view = self.convertPILRgbToAdjustedView(intrinsics, keyframe_image, keyframe_depth)
         keyframe_image = keyframe_image_view.image
         keyframe_depth = keyframe_image_view.depth
 
-        if False:
+        if plot_debug:
             depth_plot = (keyframe_depth*255/3.5).astype('uint8')
             cv2.imshow('CV Keyframe Depth',depth_plot)
 
         # Find inverse depth
         key_inv_depth = 1/keyframe_depth
-        #print("inverse depth dtype", key_inv_depth.dtype)
+        if self.debug:
+            print("inverse depth dtype", key_inv_depth.dtype)
 
         # Requirement: RGB has to np array float32, normalized in range (-0.5,0.5), with shape (1,3,240,320)
         # Get the Current RGB image from ROS message and convert it to openCV Image format
@@ -184,22 +199,24 @@ class DeepTAMTracker(object):
             print(e)
             return TrackImageResponse(0)
         
-        if test:
+        if plot_debug:
             cv2.imshow('CV Current RGB', current_image)
-            #current_image.show()
 
         # Convert CV image to PIL image
         current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
-        current_image = Image.fromarray(current_image).resize((320, 240))
+        #current_image = Image.fromarray(current_image).resize((320, 240))
+        current_image = Image.fromarray(current_image)
 
         current_image_view = self.convertPILRgbToAdjustedView(intrinsics, current_image)
         current_image = current_image_view.image
 
         # Loading prior rotation and translation
         prev_rotation = np.array(req.rotation_prior)
-        #print("prev_rotation", prev_rotation)
+        if self.debug:
+            print("prev_rotation", prev_rotation)
         prev_translation = np.array(req.translation_prior)
-        #print("prev_translation", prev_translation)
+        if self.debug:
+            print("prev_translation", prev_translation)
 
         # Loading the feed dict with converted values:
         feed_dict = {
@@ -214,9 +231,11 @@ class DeepTAMTracker(object):
         output_arrs = self.session.run(self.output, feed_dict=feed_dict)
 
         t = output_arrs['predict_translation'][0]
-        #print("t = :", t)
+        if self.debug:
+            print("t = :", t)
         R = angleaxis_to_rotation_matrix(output_arrs['predict_rotation'][0])
-        #print("R = :", R)
+        if self.debug:
+            print("R = :", R)
 
         T = np.vstack((np.hstack((R,t[:,np.newaxis])), np.asarray([0,0,0,1])))
         Tinv = np.linalg.inv(T)
@@ -224,6 +243,7 @@ class DeepTAMTracker(object):
         tinv = Tinv[:3,3]
 
         if True:
+            cv2.imshow('depth_map', output_arrs['rendered_depth'][0, 0])
             cv2.imshow('warped_img', output_arrs['warped_image'][0].transpose([1,2,0])+0.5)
             cv2.waitKey(200)
 
@@ -235,21 +255,19 @@ class DeepTAMTracker(object):
         #print("Angle axis:", output_arrs['predict_rotation'][0])
         #q = self.angle_axis_to_quaternion(output_arrs['predict_rotation'][0])
         #print("Quaternion:", q)
-        print("translation:", Vector3(*tinv))
-        
-        #tinv = np.asarray([1,2,3])
-        #Rinv = np.eye(3)
-        
-        print("Returning tracked response")
+        if self.debug:
+            print("translation:", Vector3(*tinv))
+                
+        rospy.loginfo("Returning tracked response")
         return TrackImageResponse(Transform(Vector3(*tinv), self.rotation_matrix_to_quaternion(Rinv)))
 
     def run(self):
         service = rospy.Service('track_image', TrackImage, self.track_image_cb)
-        print("Ready to track images")
+        rospy.loginfo("Ready to track images")
         try:
             rospy.spin()
         except KeyboardInterrupt:
-            print("Shutting down deepTAM tracker")
+            rospy.loginfo("Shutting down deepTAM tracker")
 
 if __name__ == "__main__":
     tracker = DeepTAMTracker()
